07899152140662e8114c6e8d5c7c4656b9ad31be
Adrian Sanabria-Diaz <sigmaenigma@users.noreply.github.com>
2024-08-31 14:07:24 -0500

    Update cloudflare_dns_record_update.py
    Cleaning up code a bit

--- cloudflare_dns_record_update.py
+++ cloudflare_dns_record_update.py
@@ -20,9 +20,9 @@ def get_verify_api():
     try:
         url = f"https://api.cloudflare.com/client/v4/user/tokens/verify"
         headers = get_headers()
-        verify_url_response = requests.get(url=url, headers=headers)
-        if "result" in verify_url_response.json():
-            if verify_url_response.json()['result']['status'] == 'active':
+        response = requests.get(url=url, headers=headers)
+        if "result" in response.json():
+            if response.json()['result']['status'] == 'active':
                 return True
             else:
                 return False
@@ -37,31 +37,35 @@ def get_headers():
     try:
         token = get_config()['token']
         headers = {
-                "Authorization": f"Bearer {token}",
-                "Content-Type": "application/json"
-            }
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json"
+        }
         return headers
     except Exception as e:
         print(f'An issue occured getting headers: {e}')
         return False
 
-def update_ip_in_cloudflare(current_public_ip, cloudflare_ip, name, id, zone_id, force=False):
+def update_ip_in_cloudflare(current_public_ip, zone_data, force=False):
     """ Updates the IP (content) in CloudFlare if a change is detected or the Force flag is enabled for a given domain name """
     try:
+        cloudflare_ip = zone_data["content"]
+        name          = zone_data["name"]
+        id            = zone_data["id"]
+        zone_id       = zone_data["zone_id"]
         if current_public_ip != cloudflare_ip or force == True:
             print(f'Updating the CloudFlare IP {cloudflare_ip} with the current public IP: {current_public_ip}')
             current_time = datetime.now()
             headers = get_headers()
             url = f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{id}"
             payload = {
-                "content": f"{current_public_ip}",
-                "name": f"{name}",
-                "proxied": False,
-                "type": "A",
-                "comment": f"IP last updated on {current_time} via API User",
-                "id": f"{id}",
-                "tags": [],
-                "ttl": 3600
+                "content":  f"{current_public_ip}",
+                "name":     f"{name}",
+                "proxied":  False,
+                "type":     "A",
+                "comment":  f"IP last updated on {current_time} via API User",
+                "id":       f"{id}",
+                "tags":     [],
+                "ttl":      3600
             }
             response = requests.patch(url=url, json=payload, headers=headers)
             if response.status_code != 200:
@@ -87,13 +91,15 @@ def get_current_public_ip():
 def get_zone_data():
     """ Pulls Zone data to be used in other functions """
     try:
+        print(f'Getting zone data... ')
         headers = get_headers()
         config = get_config()
         zone_name = config['zone_name']
         record_name = config['record_name']
         url = f"https://api.cloudflare.com/client/v4/zones/{zone_name}/dns_records?name={record_name}"
-        response = requests.get(url=url, headers=headers).json()
-        result = response["result"][0]
+        response = requests.get(url=url, headers=headers)
+        json_response = response.json()
+        result = json_response["result"][0]
         return result
     except Exception as e:
         print(f'An issue occured trying to get the zone data: {e}')
@@ -107,14 +113,7 @@ def main():
             print(f'Token active... continuing... ')
             current_public_ip = get_current_public_ip()
             zone_data = get_zone_data()
-            update_ip_in_cloudflare(
-                current_public_ip=current_public_ip, 
-                cloudflare_ip=zone_data["content"], 
-                name=zone_data["name"], 
-                id=zone_data["id"], 
-                zone_id=zone_data["zone_id"],
-                force=True
-                )
+            update_ip_in_cloudflare(current_public_ip=current_public_ip, zone_data=zone_data,force=True)
         print(f'Complete... ')
     except Exception as e:
         print(f'An issue occured trying to update the CloudFlare DNS Record: {e}')
